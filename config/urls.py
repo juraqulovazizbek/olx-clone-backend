a5def728c4da13d724f45ba9a1e129262805a59b
Juraqulov Azizbek <sen@gmail.com>
2026-03-10 09:41:43 +0000

    add urls configuration for all apps

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # API documentation
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+
+    # API endpoints
+    path('api/v1/users/', include('apps.users.urls')),
+    path('api/v1/sellers/', include('apps.sellers.urls')),
+    path('api/v1/categories/', include('apps.categories.urls')),
+    path('api/v1/products/', include('apps.products.urls')),
+    path('api/v1/favorites/', include('apps.favorites.urls')),
+    path('api/v1/orders/', include('apps.orders.urls')),
+    path('api/v1/auth/', include('apps.telegram_auth.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
